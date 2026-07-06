fae090010dc87b89ba1628baa37696928330971b
dwok09 <derekwalkerit@gmail.com>
2026-07-06 06:27:37 -0500

    fixing variable broke the formatting check. fixed the spacing

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -38,10 +38,10 @@ module "vpc" {
 }
 
 module "eks" {
-  source              = "../../modules/eks"
-  cluster_name        = "${var.environment}-eks"
-  vpc_id              = module.vpc.vpc_id
-  subnet_ids          = module.vpc.private_subnet_ids
+  source       = "../../modules/eks"
+  cluster_name = "${var.environment}-eks"
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.private_subnet_ids
 }
 
 module "workloads" {
